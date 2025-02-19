41efa052c29ac73533c177d7deecc185f4a0d911
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-02-19 23:31:11 +0530

    Recursive Approach to Display LL

---
+++ Linked-List/recursivedisplayLL.cpp
@@ -0,0 +1,32 @@
+//Displaying Linkedlist using recursion
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next ;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void recursivePrint(Node *head) {
+    if(head == NULL){
+        return ;
+    }
+
+    cout<<head -> data << " ";
+    recursivePrint(head -> next);
+}
+
+int main(){
+    Node *head = new Node(1);
+    head ->next = new Node(2);
+    head ->next ->next = new Node(3);
+    head ->next ->next ->next = new Node(4);
+
+    recursivePrint(head);
+    return 0;
+}
