1007981a94bdd362520f1ef2363cbd7461694a6b
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-12 23:02:26 +0530

    Delete the last Node from LL

--- Linked-List/deletefirstNodeLL2.cpp
+++ Linked-List/deletefirstNodeLL2.cpp
@@ -1,42 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;
+    //delete the last node from LL
+    #include<bits/stdc++.h>
+    using namespace std;
 
-class Node{
+    class Node{
+        public:
+        int data;
+        Node *next;
+        Node(int x){
+            data = x;
+            next = nullptr;
+        }
+    };
 
-    public:
-    int data;
-    Node *next;
 
-    Node(int x){
-        data = x;
-        next = NULL;
+     Node *deletefromLast(Node *head){
+        if(head == nullptr || head->next == nullptr) 
+            return nullptr;
+
+        Node *curr = head;
+        while(curr->next->next != nullptr){
+            curr = curr->next;
+        }
+
+        delete curr->next;
+        return head;
+     }
+
+     void printLL(Node* curr){
+        while(curr != nullptr ){
+            cout<<curr->data<< " ";
+            curr = curr->next;
+        }
+     }
+
+    int main(){
+
+        Node *head = new Node(10);
+        head->next = new Node(20);
+        head->next->next = new Node(30);
+        head->next->next->next = new Node(40);   
+
+        Node *curr = deletefromLast(head);
+
+        printLL(curr);
+        return 0;
     }
-};
-
-Node *deleteFirst(Node *head){
-    if(head == nullptr){
-        return nullptr;
-    }
-
-    Node *temp = head->next;
-    delete head;
-    return temp;
-}
-
-void printLL(Node *curr){
-    while(curr != nullptr){
-        cout<<curr->data<<" ";
-        curr = curr->next;
-    }
-}
-
-int main(){
-    Node *head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);     
-
-    Node *curr = deleteFirst(head);
-    printLL(curr);
-    
-    return 0;
-}
