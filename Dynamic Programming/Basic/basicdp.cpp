6fd05f1833e6e0f97e1820071a4c5b14bec51ce6
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-20 23:49:40 +0530

    Dynamic Prog -Fibonacci DP

--- Dynamic Programming/Basic/basicdp.cpp
+++ Dynamic Programming/Basic/basicdp.cpp
@@ -1,8 +1,16 @@
-//Fibonacci
+//Fibonacci Series using Dynamic Programming
 #include<iostream>
 using namespace std;
 
 int main(){
-    cout<<"Hello World";
+    int n;
+    cin>>n;
+    int dp[n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout<<dp[n];
     return 0;
 }
