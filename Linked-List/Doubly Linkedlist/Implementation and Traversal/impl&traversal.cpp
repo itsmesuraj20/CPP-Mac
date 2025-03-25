3cbb9867967cede7824d76b551951b2fc2079e2b
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-25 13:28:04 +0530

    DLL using Github Copilot

--- Linked-List/Doubly Linkedlist/Implementation and Traversal/impl&traversal.cpp
+++ Linked-List/Doubly Linkedlist/Implementation and Traversal/impl&traversal.cpp
@@ -1,28 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node{
+class Node {
+public:
     int data;
     Node *next;
     Node *prev;
-    Node(int data){
+    
+    Node(int data) {
         this->data = data;
         next = nullptr;
         prev = nullptr;
     }
 };
 
-int main(){
+class DoublyLinkedList {
+private:
+    Node *head;
 
-    Node *head = new Node(10);
-    Node *temp1 = new Node(20);
-    Node *temp2 = new Node(30);
+public:
+    DoublyLinkedList() : head(nullptr) {}
 
-    head->next = temp1;
-    temp1->prev = head;
-    temp1->next = temp2;
-    temp2->prev = temp1;
+    // Insert at beginning
+    void insertAtHead(int data) {
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        if (head != nullptr) {
+            head->prev = newNode;
+        }
+        head = newNode;
+    }
+
+    // Forward traversal
+    void printForward() {
+        if (head == nullptr) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        
+        Node *curr = head;
+        cout << "Forward Traversal: ";
+        while (curr != nullptr) {
+            cout << curr->data << " ";
+            curr = curr->next;
+        }
+        cout << endl;
+    }
+
+    // Backward traversal
+    void printBackward() {
+        if (head == nullptr) {
+            cout << "List is empty" << endl;
+            return;
+        }
+
+        Node *curr = head;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+
+        cout << "Backward Traversal: ";
+        while (curr != nullptr) {
+            cout << curr->data << " ";
+            curr = curr->prev;
+        }
+        cout << endl;
+    }
+
+    // Destructor to free memory
+    ~DoublyLinkedList() {
+        Node *curr = head;
+        while (curr != nullptr) {
+            Node *temp = curr;
+            curr = curr->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+    
+    // Insert some elements
+    dll.insertAtHead(30);
+    dll.insertAtHead(20);
+    dll.insertAtHead(10);
+
+    // Traverse both ways
+    dll.printForward();  // Output: 10 20 30
+    dll.printBackward(); // Output: 30 20 10
 
     return 0;
-    
 }
