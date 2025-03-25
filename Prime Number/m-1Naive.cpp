c70c960247922fe884ef292095ac7afa729c1799
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-25 15:07:28 +0530

    Prime Number Method-1 Naive Solution

---
+++ Prime Number/m-1Naive.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    if(n == 1) 
+    return false;
+
+    for(int i = 2;i<n;i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    cout<<"Number is : "<< n <<endl;
+
+
+    if(isPrime(n) == true)
+    cout<<n<<" is Prime Number"<<endl;
+
+    else
+    cout<<n<<" is not Prime Number"<<endl;
+
+    return 0;
+}
