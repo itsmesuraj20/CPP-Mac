96878339f070ecfc7620b0e25c16bf753fe325ea
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-26 23:57:59 +0530

    Hashing

---
+++ Hashing/Implementation/hashingimple.cpp
@@ -0,0 +1,12 @@
+//Hashing Implementation
+//Hashing is a technique that is used to uniquely identify a specific object from a group of similar objects. Some of the hashing techniques are:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Hashing Implementation"<<endl;
+
+    return 0;
+
+}
