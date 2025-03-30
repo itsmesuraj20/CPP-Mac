37dd25be1db9160ecae3c3c6f0031c10a2b63f2c
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-30 21:58:28 +0530

    Advance DP

--- Advance Dynamic Programming/advanceDP.cpp
+++ Advance Dynamic Programming/advanceDP.cpp
@@ -2,3 +2,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int maxSum = arr[0];
+    int currentSum = arr[0];
+
+    for(int i=1;i<n;i++){
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+
+    cout << "Maximum sum of contiguous subarray: " << maxSum << endl;
+
+    return 0;
+}
