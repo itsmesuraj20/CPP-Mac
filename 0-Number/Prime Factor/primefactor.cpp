845f9b5559f7c98a0a199abd60b8772e1c6e84cb
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-04-03 23:41:07 +0530

    Prime Number

---
+++ 0-Number/Prime Factor/primefactor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void primeFactor(int n){
+    for(int i = 2; i < n ; i++ ){
+        if(isPrime(i)){
+            int x = i;
+            while(n % x == 0){
+                cout<<i<<endl;
+                x = x * i;
+            }
+        }
+    }
+}
+
+int main(){
+    int n ;
+    cout<<"Enter the value : "<<endl;
+    cin>>n;
+    cout<<n<<" : enter by you " <<endl;
+
+    primeFactor(n);
+    
+
+}
