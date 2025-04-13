2128d1e607a684f843940436629362614a50496a
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-04-13 22:20:53 +0530

    Monotonoic Stack

---
+++ Monotonoic Stack and Monotonoic Queue/Monotonoic Stack/monostack.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Monotonic Stack
+// Monotonic Stack is a stack that is either increasing or decreasing   
+
+
+// It is used to find the next greater element, next smaller element, previous greater element, previous smaller element
+// It is used to find the maximum area of histogram, maximum rectangle in a binary matrix
+
+int main(){
+    cout<<"Monotonic Stack"<<endl;
+    return 0;
+}
