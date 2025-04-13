71fdeb8689cb4bb903cae61fb56f30fe1ccea1f2
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-04-13 22:22:38 +0530

    Monotonoic Queue

---
+++ Monotonoic Stack and Monotonoic Queue/Monotonoic Queue/monoqueue.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Monotonic Queue
+// Monotonic Queue is a queue that is either increasing or decreasing
+// It is used to find the maximum in a sliding window   
+
+
+int main(){
+    cout<<"Monotonic Queue"<<endl;
+    return 0;
+}
