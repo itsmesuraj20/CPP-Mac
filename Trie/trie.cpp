c874e7300c1a52272b224f06f9381e0155449c86
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-04 23:56:49 +0530

    Tries

---
+++ Trie/trie.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+public: 
