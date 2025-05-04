c69c78337bcf46465fedc9d4e01e6dd98c380171
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-04 23:57:16 +0530

    Tries

--- Trie/trie.cpp
+++ Trie/trie.cpp
@@ -59,3 +59,41 @@ public:
     void remove(string word) {
         removeHelper(root, word, 0);
     }
+
+    bool removeHelper(TrieNode* node, string word, int depth) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (depth == word.size()) {
+            if (node->isEndOfWord) {
+                node->isEndOfWord = false;
+                return node->isLeaf();
+            }
+            return false;
+        }
+        int index = word[depth] - 'a';
+        if (removeHelper(node->children[index], word, depth + 1)) {
+            delete node->children[index];
+            node->children[index] = nullptr;
+            return !node->isEndOfWord && node->isLeaf();
+        }
+        return false;
+    }
+    bool isLeaf() {
+        for (int i = 0; i < 26; i++) {
+            if (children[i] != nullptr) {
+                return false;
+            }
+        }
+        return true;
+    }
+    void display(TrieNode* node, string str) {
+        if (node->isEndOfWord) {
+            cout << str << endl;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (node->children[i] != nullptr) {
+                display(node->children[i], str + char(i + 'a'));
+            }
+        }
+    }
