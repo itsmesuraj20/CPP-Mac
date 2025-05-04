668a9b87213ee681ee697349f32a3c2d8580bd61
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-04 23:56:57 +0530

    Tries

--- Trie/trie.cpp
+++ Trie/trie.cpp
@@ -18,3 +18,30 @@ class Trie {
 private:
     TrieNode* root;
 public: 
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (node->children[index] == nullptr) {
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (node->children[index] == nullptr) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEndOfWord;
+    }
