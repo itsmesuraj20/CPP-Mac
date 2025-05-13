de91088ccf92f85b7c7bd13746527df70e9e85e6
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-13 15:29:58 +0530

    Tree

--- Tree/Basic Implementation/inOrder.cpp
+++ Tree/Basic Implementation/inOrder.cpp
@@ -28,8 +28,11 @@ int main(){
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
+    // Left Subtree 
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    
+    //Right Subtree
     root->right->left = new Node(6);
     root->right->right = new Node(7); 
 
