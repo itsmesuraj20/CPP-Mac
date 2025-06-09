3d4b5eed8c2a8460128c7028f436b554314e7851
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-06-09 12:30:54 +0530

    LinkedList Revising

--- 2-Linked-List/Implementation/implementationofLL.cpp
+++ 2-Linked-List/Implementation/implementationofLL.cpp
@@ -14,12 +14,12 @@ class Node{
 int main(){
 
     
-    //adding the value to the Linked List (Long way of add the value)
-    Node *head = new Node(10);
-    Node *temp1 = new Node(20);
-    Node *temp2 = new Node(30);
-    head ->next = temp1;
-    temp1 ->next = temp2;
+    // //adding the value to the Linked List (Long way of add the value)
+    // Node *head = new Node(10);
+    // Node *temp1 = new Node(20);
+    // Node *temp2 = new Node(30);
+    // head ->next = temp1;
+    // temp1 ->next = temp2;
     //                                  OR    
 
     //adding the value to the Linked List (Short way of add the value)
@@ -30,8 +30,10 @@ int main(){
     //traversal for the linked list
     Node *curr = head;
     while(curr != NULL){
-        cout<<curr->data<<"_";
+        
+        cout<< curr->data << " ";
         curr = curr->next;
+    
     }
 
     return 0;
