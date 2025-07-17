0fb9e1995866ad09b95803059876fc2b0af29f7d
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-17 20:14:16 +0530

    Array- isArrayIsSorted

--- 1-Array/question/isArrayisSorted.cpp
+++ 1-Array/question/isArrayisSorted.cpp
@@ -3,12 +3,30 @@ using namespace std;
 
 
 int main(){
-    int arr[] = { 1,5,6,7,2,3,9};
-
-    for (int i = 0; i < count; i++)
-    {
-        /* code */
-    }
     
+    int arr1[] = {1,5,6,7,2,3,9};
+    int n = sizeof(arr1)/sizeof(arr1[0]);
 
+    cout<<n<<endl;
+
+    //handle edge cases
+    if(n == 0){
+        cout<<"Array is Empty";
+    }
+    else if(n == 1){
+        cout<<"Array is Sorted";
+    }
+
+    else{
+    bool result = true;
+    for (int i = 1; i < n; i++)
+    {
+       if(!(arr1[i] >= arr1[i-1])){
+        result = false;
+        break;
+       }
+    }
+    if(result) cout<<"Sorted in ascending order or Non-descending order" << endl;
+    else cout<<"Not Sorted" <<endl;
+    }
 }
