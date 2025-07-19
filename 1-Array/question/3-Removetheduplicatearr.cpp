ad499f5be7f3edfc9c9e80776236d9cff5043a4b
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-19 23:52:41 +0530

    Remove the duplicate

--- 1-Array/question/3-Removetheduplicatearr.cpp
+++ 1-Array/question/3-Removetheduplicatearr.cpp
@@ -1,10 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1703906517/
+/*
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+*/
+
 int main(){
+
+    vector<int> arr[] = [1,1,2];
+
     
-
-
+    
 
     return 0;
 }
