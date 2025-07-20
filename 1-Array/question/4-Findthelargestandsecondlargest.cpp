549f20a191efa0044111f603a06f183851589bf2
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-20 14:43:23 +0530

    Findthelargestandsecondlargest

---
+++ 1-Array/question/4-Findthelargestandsecondlargest.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr = {1,2,3};
+
+    cout<<"Largest in the array : "<<largest(arr)<<endl;
+    cout<<"Second Smallest in the array : "<<secondLargest(arr)<<endl;
+
+}
