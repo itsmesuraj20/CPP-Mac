e59fda46dfc606a607583190c95e4224c5cd3e43
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-21 22:38:09 +0530

    Findthelargestandsecondlargest

--- 1-Array/question/4-Findthelargestandsecondlargest.cpp
+++ 1-Array/question/4-Findthelargestandsecondlargest.cpp
@@ -1,10 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int largestBruteMethod(vector<int> &arr){
+    sort(arr.begin(),arr.end());
+
+    return arr.back();
+
+    //TC- O(nlogn) :  Because of Sorting
+    //SC - O(1)
+}
+
+int secondLargestBruteMethod(vector<int> &arr){
+    int largestSoFar = largestBruteMethod(arr);
+    for(int i = arr.size()-2;i>=0 ;i--){
+        if(arr[i] != arr[arr.size()-1]){
+            return arr[i];
+
+        }
+    }
+
+    //TC - O(n)
+    //SC- 0(1)
+    return -1;
+}
+
+int largestBetter(vector<int> arr){
+    
+    return 0;
+}
+
+int secondLargestBetter(vector<int> arr){
+
+
+    return 0;
+}
+
+int largestOptimal(vector<int> arr){
+
+    return 0;
+}
+
+int secondLargestOptimal(vector<int> arr){
+
+}
+
 int main(){
     vector<int> arr = {1,2,3};
 
-    cout<<"Largest in the array : "<<largest(arr)<<endl;
-    cout<<"Second Smallest in the array : "<<secondLargest(arr)<<endl;
+    cout<<"Brute Method" << endl;
+    
+    cout<<"Largest in the array : " << largestBruteMethod(arr) << endl;
+    cout<<"Second Smallest in the array : " << secondLargestBruteMethod(arr) << endl;
+
+    cout<<"Better Method" << endl;
+
+    cout<<"Largest in the array in the better " << largestBetter(arr) << endl;
+    cout<<"Second Smallest in the array better " << secondLargestBetter(arr) << endl;
+
+    cout<<"Optimize Method" << endl;
+
+    cout<<"Largest in the array in the optimized " << largestOptimal(arr) << endl;
+    cout<<"Second Smallest in the array " << secondLargestOptimal(arr) << endl;
+
 
 }
