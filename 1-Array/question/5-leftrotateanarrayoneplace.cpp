3c9d4e4e992e68432d01a3ae4497c1cbb97dffc8
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-26 00:35:15 +0530

    LeftRotateanArraybyOneShift

---
+++ 1-Array/question/5-leftrotateanarrayoneplace.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+/*
+Question : Left Rotate an array one place
+
+*/
+
+int main(){
+    std::vector<int> arr = {1,2,3,4,5};
+
+    int temp = arr[0];
+
+    for(int i = 1; i<arr.size();i++){
+        arr[i-1] = arr[i];
+    }
+
+    arr[arr.size()- 1] = temp;
+
+    for(int i : arr){
+        std::cout<<i<<" ";
+    }
+
+    //TC - O(n)
+    //SC - O(1)
+}
