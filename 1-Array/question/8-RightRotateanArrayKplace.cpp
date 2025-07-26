04eba46b1ce99003d9d71118410ff1559c2fa4da
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-26 23:56:52 +0530

    RightRotateanArrayKPlace

--- 1-Array/question/8-RightRotateanArrayKplace.cpp
+++ 1-Array/question/8-RightRotateanArrayKplace.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
+// arr[] = {1,2,3,4,5,6,7} , d = 2
+// O/P = {6,7,1,2,3,4,5}
+
 
 vector<int> bruteRightRotate(vector<int> arr ,int d ){
     vector<int> temp;
     int n = arr.size();
+    d=d%n;
     int end = n-d;
 
     for(int i = 0;i<end;i++){
@@ -25,10 +29,20 @@ vector<int> bruteRightRotate(vector<int> arr ,int d ){
     
     return arr;
 }
+
+vector<int> optimalRotate(vector<int> arr,int d){
+    int n = arr.size();
+    d=d%n;
+    reverse(arr.begin(),arr.end() - d);
+    reverse(arr.end() - d ,arr.end());
+    reverse(arr.begin(),arr.end());
+}
 int main(){
     //  vector<int> arr = {1,2,3,4,5,6,7,8,9,0};
     //  vector<int> arr = {1,2,3,4,5,6,7};
-     vector<int> arr = {-1,-100,3,99};
+    //  vector<int> arr = {-1,-100,3,99};
+
+    vector<int> arr = {1,2,3,4,5};
      int d = 2;
 
     vector<int> newArr = bruteRightRotate(arr,d);
--- 1-Array/question/revision.cpp
+++ 1-Array/question/revision.cpp
@@ -1,27 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
-    //  vector<int> arr = {1,2,3,4,5,6,7,8,9,0};
-     vector<int> arr = {1,2,3,4,5};
+    vector<int> arr = {1,2,3,4,5};
     int d = 2;
 
-    vector<int> temp;
-    int n = arr.size();
-    int end = n-d;
-    for(int i = 0;i<end;i++){
-        temp.push_back(arr[i]);
-    }
+    reverse(arr.begin(),arr.end() - d);
+    reverse(arr.end() - d ,arr.end());
 
-    int j = 0;
-    for(int i = 0 ;i<n;i++){
-       arr[j] = arr[i];
-        j++;
-    }
 
     for(int i : arr){
         cout<<i<<" ";
     }
 
-    return 0;
 }
+
+// int main(){
+//     //  vector<int> arr = {1,2,3,4,5,6,7,8,9,0};
+//      vector<int> arr = {1,2,3,4,5};
+//     int d = 2;
+
+//     vector<int> temp;
+//     int n = arr.size();
+//     int end = n-d;
+//     for(int i = 0;i<end;i++){
+//         temp.push_back(arr[i]);
+//     }
+
+//     int j = 0;
+//     for(int i = 0 ;i<n;i++){
+//        arr[j] = arr[i];
+//         j++;
+//     }
+
+//     for(int i : arr){
+//         cout<<i<<" ";
+//     }
+
+//     return 0;
+// }
