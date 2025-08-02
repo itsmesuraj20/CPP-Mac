67a15f56667aa1fca5c4806102df96167bbcded0
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-08-02 23:05:14 +0530

    Unionofthetwosortedarray

---
+++ 1-Array/question/11-Unionofthetwosortedarray.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+/*
+Find the union of the two sorted array :
+arr1 = [1,2,3,4,5,5,6];
+arr2 = [2,3,3,4,4,5];
+
+o/p = [1,2,3,4,5,6];
+
+*/
+
+vector<int> union_Brute(vector<int> arr1,vector<int> arr2){
+    vector<int> ans ;
+
+    int n = arr1.size(); 
+    int m = arr2.size();
+
+    set<int> s;
+
+    for (int i = 0; i < n; i++)
+    {
+       s.insert(arr1[i]);
+    }
+
+    for(int j = 0 ;j<m ; j++){
+        s.insert(arr2[j]);
+    }
+
+    for(auto i : s){
+        ans.push_back(i);
+    }
+    
+    return ans;
+}
+
+vector<int> union_Optimal(vector<int> arr1,vector<int> arr2){
+
+    vector<int> ans;
+
+    int n = arr1.size(); 
+    int m = arr2.size();
+    int i = 0, j = 0;
+
+
+    while(i< n && j<m){
+        if(arr1[i] <= arr2[j]) {
+            if(ans.empty() || ans.back() != arr1[i]){
+                ans.push_back(arr1[i]);
+                
+            }
+            i++;
+        }
+        else{
+           if(ans.empty() || ans.back() != arr2[j]){
+                ans.push_back(arr2[j]);
+            }
+            j++;
+        }
+    } 
+
+    while (i < n) {
+        if (ans.empty() || ans.back() != arr1[i]) {
+            ans.push_back(arr1[i]);
+        }
+        i++;
+    }
+
+    while (j < m) {
+        if (ans.empty() || ans.back() != arr2[j]) {
+            ans.push_back(arr2[j]);
+        }
+        j++;
+    }
+
+    return ans;
+    
+}
+
+
+
+int main(){
+    vector<int> arr1 = {1,2,3,4,5,5,6};
+    vector<int> arr2 = {2,3,3,4,4,5};
+    
+    cout<<"Size of both array is :" << arr1.size() + arr2.size()<<endl;
+    // vector<int> arr1 = {};
+    // vector<int> arr2 = {};
+
+
+    vector<int> ans = union_Optimal(arr1,arr2);
+    cout<<"Size of the array is :" << ans.size()<<endl;
+    for(int i :  ans){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
