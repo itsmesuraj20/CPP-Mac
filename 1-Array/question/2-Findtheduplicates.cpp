f866042c67ed5328211f96dc571da7743396bc84
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-08-02 14:00:23 +0530

    Findtheduplicate

--- 1-Array/question/2-Findtheduplicates.cpp
+++ 1-Array/question/2-Findtheduplicates.cpp
@@ -10,30 +10,45 @@ From coding Ninjas
 
 */
 
-int checkBrute(vi arr){
+int checkBrutev1(vi arr){
     int n = arr.size();
-
-   unordered_map<int,int> mp;
-    for(int i = 0 ; i<n;i++){
-        mp[arr[i]]++;
-    }
-
-    for(auto  i : mp){
-        if(i.second != 1){
-            return i.first;
+    int ans = -1;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i+1; j<n ; j++){
+            if(arr[i] == arr[j]){
+                return ans;
+            }
         }
     }
     return -1;
-
-    //TC : O(n);
-    //SC : O(n);
-
 }
 
-int main(){
-    vi arr = {5,1,2,3,4,0 ,0};
 
-    int ans = checkBrute(arr);
+// int checkBrutev2(vi arr){
+//     int n = arr.size();
+
+//    unordered_map<int,int> mp;
+//     for(int i = 0 ; i<n;i++){
+//         mp[arr[i]]++;
+//     }
+
+//     for(auto  i : mp){
+//         if(i.second != 1){
+//             return i.first;
+//         }
+//     }
+//     return -1;
+
+//     //TC : O(n);
+//     //SC : O(n);
+
+// }
+
+int main(){
+    // vi arr = {5,1,2,3,4,4,9,0};
+    vi arr = {1,2,3,4,5};
+
+    int ans = checkBrutev1(arr);
 
     if(ans == -1){
         cout<<"No Duplicate found";
