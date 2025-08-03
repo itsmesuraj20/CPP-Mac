b51c4adcddf4f7031e9e56860ae46c85d52d0bc9
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-08-03 13:43:37 +0530

    Intersection

---
+++ 1-Array/question/12-Intersectionoftwosorted.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    vector<int> arr1 = {1,2,3,4,5,6}; 
+    vector<int> arr2 = {1,2,3,4,7,8};
+
+    vector<int> ans;
+    int m = arr1.size(); 
+    int n = arr2.size();
+
+   vector<int> vis(n,0);
+
+   for (int i = 0; i < m; i++)
+   {
+    for(int j = 0 ; j<n ; j++){
+        if(arr1[i] == arr2[j] && vis[j] == 0){
+            ans.push_back(arr1[i]);
+            vis[j] = 1;
+            break;
+        }
+        if(arr2[j] > arr1[i]) break;
+    }
+   }
+   
+   for(int i : ans){
+    cout<<i<<" ";
+   }
+
+    return 0;
+}
