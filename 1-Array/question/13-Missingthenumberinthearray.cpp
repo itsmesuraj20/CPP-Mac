43d2832baf2a971ff8557c7f76ffdf7929240092
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-08-22 00:12:34 +0530

    Missingthenumberinthearray

--- 1-Array/question/13-Missingthenumberinthearray.cpp
+++ 1-Array/question/13-Missingthenumberinthearray.cpp
@@ -62,21 +62,56 @@ void optimal_missingnum(vector<int> &arr){
         s2 += arr[i];
     }
 
-    cout<<(totalSum - s2);
+    cout<<(totalSum - s2)<<endl;
+
+}
+
+void optimal_missingnum2(vector<int> &arr){
+    //Using two loop
+    //TC - O(2N) , SC=O(1);
+    int XOR1 = 0;
+    int n = arr.size();
+
+    for(int i = 1; i<n;i++){
+        XOR1 = XOR1^i;
+    }
+    int XOR2=0;
+
+    for(int j = 0; j<n-1;j++){
+        XOR2 = XOR2 ^ arr[j];
+    }
+
+    int  ans =  (XOR1 ^ XOR2);
+    cout<<ans;
+
+}
+
+void optimal_missingnum3(vector<int> &arr){
+    //Reduced to one loop
+    //TC - O(n) , SC=O(1);
+    int n = arr.size() + 1;  // because 1 number is missing
+    int xorAll = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        xorAll ^= arr[i];      // XOR with array element
+        xorAll ^= (i + 1);     // XOR with 1..n-1
+    }
+
+    xorAll ^= n;  // finally XOR with n
+    cout << xorAll;
 
 }
 
 int main(){
 
     // vector<int> arr = {0,1,2,4,5};
-    // vector<int> arr = {3,0,1};
-    vector<int> arr = {0,2,3,4,5};
+    vector<int> arr = {3,0,1};
+    // vector<int> arr = {0,2,3,4,5};
 
     brute_missingnum(arr);
     better_missingnum(arr);
     optimal_missingnum(arr);
-
-
+    optimal_missingnum2(arr);
 
     return 0;
 }
