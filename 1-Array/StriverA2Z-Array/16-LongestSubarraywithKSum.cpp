e8aa7142d1de15ea2fffce5182f7e0e71aa535f6
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-08-25 02:17:19 +0530

    GenerateSubArray

--- 1-Array/StriverA2Z-Array/16-LongestSubarraywithKSum.cpp
+++ 1-Array/StriverA2Z-Array/16-LongestSubarraywithKSum.cpp
@@ -24,14 +24,37 @@ void generateSubArrayBruteMethod(vector<int> &arr){
 }
 
 
+void generateSubArrayRecursively(vector<int>& arr, int start, int end) {
+
+    // Stop if we have reached the end of the array
+    if (end == arr.size())
+        return;
+
+    // Increment the end point and reset the start to 0
+    else if (start > end)
+        generateSubArrayRecursively(arr, 0, end + 1);
+
+    // Print the subarray and increment the starting point
+    else {
+        for (int i = start; i <= end; i++)  
+            cout << arr[i] << " ";
+
+        cout << endl;
+        
+        generateSubArrayRecursively(arr, start + 1, end);  
+    }
+}
+
+
 
 int main(){
     vector<int> arr = {1,2,3,1,1,1,1,4,2,3};
 
     int k;
 
-    cout<<arr.size()<<endl;
-    generateSubArrayBruteMethod(arr);
+    // cout<<arr.size()<<endl;
+    // generateSubArrayBruteMethod(arr);
+    generateSubArrayRecursively(arr, 0 , 0);
 
     return 0;
 }
