06f78e9040bd90e2f7de820e742f12f3332c46d6
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-09-05 00:52:24 +0530

    GeneratetheSubarray

--- 1-Array/StriverA2Z-Array/1-GeneratetheSubarray.cpp
+++ 1-Array/StriverA2Z-Array/1-GeneratetheSubarray.cpp
@@ -1,30 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void generateSubArrayBrute(vector<int> &arr){
+void generateSubArrayBrute(vector<int> &arr)
+{
     int n = arr.size();
+    cout << "Size : " << n << endl;
 
     for (int i = 0; i < n; i++)
     {
         for (int j = i; j < n; j++)
         {
-             for (int k = i; k <= j; k++)
+            for (int k = i; k <= j; k++)
                 cout << arr[k] << " ";
             cout << endl;
         }
-        
     }
-    
 }
 
-
-
-int main(){
-    vector<int> arr = {1,2,3,1,1,1,1,4,2,3};
-   
+int main()
+{
+    // vector<int> arr = {1,2,3,1,1,1,1,4,2,3};
+    vector<int> arr = {1, 2, 3};
 
     generateSubArrayBrute(arr);
-
 
     return 0;
 }
