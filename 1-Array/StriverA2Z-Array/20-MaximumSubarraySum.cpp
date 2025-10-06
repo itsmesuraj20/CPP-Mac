579d0c572f409eb65af2431ea9e16e0617f46f13
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-10-07 00:57:11 +0530

    MaximumSubarraySum

--- 1-Array/StriverA2Z-Array/20-MaximumSubarraySum.cpp
+++ 1-Array/StriverA2Z-Array/20-MaximumSubarraySum.cpp
@@ -1,6 +1,6 @@
-//LC - https://leetcode.com/problems/maximum-subarray/description/
+// LC - https://leetcode.com/problems/maximum-subarray/description/
 
-//Algorithm used :  Kadane Algorithm
+// Algorithm used :  Kadane Algorithm
 
 /*
 
@@ -10,45 +10,100 @@ Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 #define endl "\n"
 
-int brute_MaximumSubArray(vector<int> &arr){
+int brute_MaximumSubArray(vector<int> &arr)
+{
     int n = arr.size();
 
     int maxi = INT_MIN;
-    for(int i = 0; i< n ; i++){
-        for(int j = 0; j<n ;j++){
-            int sum = 0 ;
-            for (int k = i; k <= j ; k++)
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int sum = 0;
+            for (int k = i; k <= j; k++)
             {
                 sum += arr[k];
             }
-            maxi = max(maxi,sum);
+            maxi = max(maxi, sum);
         }
     }
 
     return maxi;
 }
 
-int better_MaximumSubArray(vector<int> &arr){
+int better_MaximumSubArray(vector<int> &arr)
+{
+    int n = arr.size();
 
+    int maxi = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += arr[j];
+        }
+        maxi = max(maxi, sum);
+    }
+
+    return maxi;
 }
 
-int optimal_MaximumSubArray(vector<int> &arr){
 
+int optimal_MaximumSubArray(vector<int> &arr)
+{
+    //Kadane Algorithm 
+    int maxi = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+
+        if(sum > maxi){
+            maxi = sum ;
+        }
+        if(sum < 0) sum = 0;
+    }
+
+    return maxi;
+    
+}
+
+void unknown(vector<int> &arr){
+    //Kadane Algorithm 
+    int maxi = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(sum == 0) int start = i;
+        sum += arr[i];
+
+        if(sum > maxi){
+            maxi = sum ;
+            int ansStart = start, ansEnd = i;
+        }
+        if(sum < 0) sum = 0;
+    }
+
+    return maxi;   
 }
 
 
-int main(){
+int main()
+{
 
-    // vector<int> arr = { -2,1,-3,4,-1,2,1,-5,4 };
-    vector<int> arr = {5,4,-1,7,8};
+    vector<int> arr = { -2,1,-3,4,-1,2,1,-5,4 };
+    // vector<int> arr = {5, 4, -1, 7};
 
-    cout<<brute_MaximumSubArray(arr);
+    // cout << brute_MaximumSubArray(arr);
     // cout<<better_MaximumSubArray(arr);
-    // cout<<optimal_MaximumSubArray(arr);
+    cout<<optimal_MaximumSubArray(arr);
 
     return 0;
 }
