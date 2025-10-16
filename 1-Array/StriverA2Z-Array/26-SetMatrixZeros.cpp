bda17d5007318b24fb6aee39128fa9f3890766f7
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-10-16 22:54:02 +0530

    SetMatrixZero

--- 1-Array/StriverA2Z-Array/26-SetMatrixZeros.cpp
+++ 1-Array/StriverA2Z-Array/26-SetMatrixZeros.cpp
@@ -19,20 +19,20 @@ int cols = arr[0].size();
 ˀ
 */
 
-void markRow(vector<int>> &arr, int i ,int m ){
+void markRow(vector<vector<int>> &arr, int row,int m ){
     for (int i = 0; i < m; i++)
     {
-        if(arr[i][j] != 0){
-            curr[i][j] = -1; // Marking , not zero immediately.
+        if(arr[row][j] != 0){
+            arr[row][j] = -1; // Marking , not zero immediately.
         }
     }
 }
 
-markCol(vector<int>> &arr, int j , int n){
+markCol(vector<vector<int>> &arr, int cols , int n){
     for (int i = 0; i < n; i++)
     {
-        if(arr[i][j] != 0)
-        arr[i][j] = -1; // Marking , not zero immediately.
+        if(arr[i][col] != 0)
+        arr[i][col] = -1; // Marking , not zero immediately.
     }
     
 }
@@ -47,18 +47,41 @@ void brute(vector<vector<int>> &arr){
         for (int j = 0; j < n; j++)
         {
             if(arr[i][j] == 0){
-                markRow(i);
-                markCol(j);
+                markRow(arr, i, n);
+                markCol(arr, j, m);
             }
         }    
     }
 
-    for()
+    for(int i = 0;i < m; i++){
+        for (int j = 0; j < n; j++)
+        {
+            if(arr[i][j] == -1){
+                arr[i][j] = 0;
+            }
+        }   
+    }
+}
 
-
+void printMatrix(vector<vector<int>> &arr){
+    for(auto &row : arr){
+        for(int val : arr){
+            cout<< val << " ";
+        }
+        cout<<endl;
+    }
 }
 
 int main(){
-    brute();
+    vector<vector<int>> arr = {{1, 2, 3},{4, 0, 6},{7, 8, 9}};
+
+    cout<<"Original Matrix: \n";
+    printMatrix(arr);
+
+    brute(arr);
+    
+    cout<<"\n After Set Matrix Zero (Brute) : \n";
+    printMatrix(arr);
+
     return 0;
 }
