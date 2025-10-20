b6f743e6cc08da815009a6b6873ea841786c5932
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-10-20 16:22:56 +0530

    Little Tweak

---
+++ String/Basic/2-Basic.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string var;
+    getline(cin , var);
+
+
+    cout<<var[0]<<" "<<var[1]<<" "<<var[2]<<" "<<var[3];
+
+
+    return 0;
+}
