aac99705f58d0fc67292561ea3a28bfeb91f20d2
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-10-23 15:54:29 +0530

    SetMatrixZero

--- StriverA2Z-Array/26-SetMatrixZeros.cpp
+++ StriverA2Z-Array/26-SetMatrixZeros.cpp
@@ -18,36 +18,150 @@ int rows = arr.size();
 int cols = arr[0].size();
 ˀ
 */
-void markRow(vector<vector<int>> &arr, int row,int m ){
+// void markRow(vector<vector<int>> &arr, int row,int m ){
+//     for (int j = 0; j < m; j++)
+//     {
+//         if(arr[row][j] != 0){
+//             arr[row][j] = -1; // Marking , not zero immediately.
+//         }
+//     }
+// }
+
+// void markCol(vector<vector<int>> &arr, int col , int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         if(arr[i][col] != 0)
+//         arr[i][col] = -1; // Marking , not zero immediately.
+//     }
+// }
+
+
+void optimal(vector<vector<int>> &arr){
+    int m = arr.size();
+    int n = arr[0].size();
+
+    int firstRowZero = 0,firstColZero = 0;
+
+    //Step-1 :Check the first row and col
+    for (int j = 0; j < n; j++)
+    {
+        if(arr[0][j] == 0)
+           firstRowZero = 1;
+    }
+
     for (int i = 0; i < m; i++)
     {
-        if(arr[row][j] != 0){
-            arr[row][j] = -1; // Marking , not zero immediately.
+        if(arr[i][0] == 0)
+            firstColZero = 1;
+    }
+    
+    //Step-2 :use first row and col as markers
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if(arr[i][j] == 0){
+                arr[i][0] = 0;
+                arr[0][j] = 0;
+            }
         }
+    }
+    //Step-3 :use set zero based on markers
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if(arr[i][0] == 0 || arr[0][j] == 0){
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    //Step-4 :handle first row and col
+    if(firstRowZero == 1){ //Row
+        for (int j = 0; j < n; j++)
+        {
+            arr[0][j] = 0;
+        }
+    }
+
+     if(firstColZero == 1){
+        for (int i = 0; i < m; i++)
+        {
+            arr[i][0] = 0;
+        }
+    }
+
+
+    //Step-5 :Output
+    cout<<"\n After Set Matrix Zero : \n";
+      for(int i = 0 ; i < m ;i++){
+        for(int j = 0; j< n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    // for(auto x : arr){
+    //     for(auto i : x){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+}
+
+
+
+void better(vector<vector<int>> &arr){
+
+    int m = arr.size();//row
+    int n = arr[0].size();//col
+
+    vector<int> row(m,0),col(n,0);
+
+    for(int i = 0 ; i < m ;i++){
+        for(int j = 0; j< n;j++){
+            if(arr[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+   for(int i = 0 ; i < m ;i++){
+        for(int j = 0; j< n;j++){
+            if(row[i] || col[j])
+            arr[i][j] = 0 ;
+        }
+    }
+
+
+     cout<<"\n After Set Matrix Zero (Brute) : \n";
+      for(int i = 0 ; i < m ;i++){
+        for(int j = 0; j< n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
     }
 }
 
-markCol(vector<vector<int>> &arr, int cols , int n){
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i][col] != 0)
-        arr[i][col] = -1; // Marking , not zero immediately.
-    }
-    
-}
+
 
 void brute(vector<vector<int>> &arr){
     int m = arr.size();
     int n = arr[0].size();
     
-    
-    for (int i = 0; i < m; i++)
+    vector<int> markRow(m,0) , markCol(n,0);
+
+    for (int row = 0; row < m; row++)
     {
-        for (int j = 0; j < n; j++)
+        for (int col = 0; col < n; col++)
         {
-            if(arr[i][j] == 0){
-                markRow(arr, i, n);
-                markCol(arr, j, m);
+            if(arr[row][col] == 0){
+                markRow[row] = 1, markCol[col] = 1;
+                // markRow(arr, row, n);//Opposite for the third paramater
+                // markCol(arr, col, m);//Opposite for the third paramater
             }
         }    
     }
@@ -55,17 +169,21 @@ void brute(vector<vector<int>> &arr){
     for(int i = 0;i < m; i++){
         for (int j = 0; j < n; j++)
         {
-            if(arr[i][j] == -1){
+
+            if(markRow[i] || markCol[j]){
                 arr[i][j] = 0;
             }
+            // if(arr[i][j] == -1){
+            //     arr[i][j] = 0;
+            // }
         }   
     }
 }
 
 void printMatrix(vector<vector<int>> &arr){
-    for(auto &row : arr){
-        for(int val : arr){
-            cout<< val << " ";
+    for(auto &i : arr){
+        for(auto it : i){
+            cout<< it << " ";
         }
         cout<<endl;
     }
@@ -74,15 +192,19 @@ void printMatrix(vector<vector<int>> &arr){
 int main()
 {
 
-    vector<vector<int>> arr = {{1, 2, 3},{4, 0, 6},{7, 8, 9}};
+    vector<vector<int>> arr = {{1,1,1,1},{1,0,0,1},{1,1,0,1},{1,1,1,1}};
+    // vector<vector<int>> arr = {{-1},{2},{3}};
 
     cout<<"Original Matrix: \n";
     printMatrix(arr);
 
-    brute(arr);
+    // brute(arr);
+    // better(arr);
+    optimal(arr);
+
     
-    cout<<"\n After Set Matrix Zero (Brute) : \n";
-    printMatrix(arr);
+    // cout<<"\n After Set Matrix Zero (Brute) : \n";
+    // printMatrix(arr);
 
     return 0;
 }
