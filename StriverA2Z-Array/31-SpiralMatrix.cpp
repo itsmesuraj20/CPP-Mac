16863f36d8e54af4b2e6e3c2e7b4a79fca52ea06
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-11-28 23:58:20 +0530

    Spiral

--- StriverA2Z-Array/31-SpiralMatrix.cpp
+++ StriverA2Z-Array/31-SpiralMatrix.cpp
@@ -4,48 +4,54 @@ using namespace std;
 //As it has only one solution 
 
 //Spiral Matrix
-vector<vector<int>> spiralmatrixBuilderFunction(){
-
-}
-
-int main(){
-
-vector<vector<int>> arr = {{1,2,3},{4,5,6},{7,8,9}};
-int rows = arr.size();
+vector<int> spiralmatrixBuilderFunction(vector<vector<int>> &arr){
+    int rows = arr.size();
 int cols = arr[0].size();
-vector<vector<int>> ans;
+vector<int> ans;
     //top , bottom , left , right 
 
     int top = 0, left = 0;
-    int bottom = row - 1 , right =  cols - 1;
-while(top<= bottom || left <= right)
+    int bottom = rows - 1 , right =  cols - 1;
+while(top<= bottom && left <= right)
 {
     for (int i = left; i <=right; i++)
     {
         ans.push_back(arr[top][i]);
     }
-
     top++;
 
     for (int i = top; i <= bottom; i++)
     {
        ans.push_back(arr[i][right]);
     }
-
     right--;
 
+    if (top <= bottom){
     for(int i = right ; i >= left ; i--){
         ans.push_back(arr[bottom][i]);
     }
     bottom-- ;
-
-    for(int i= bottom ; i >= top ; i++ ){
-        ans.push_back(arr[left][i]);
     }
-
+    if(left <= right){
+    for(int i= bottom ; i >= top ; i++ ){
+        ans.push_back(arr[i][left]);
+    }
     left++;
+  }
 }
     
+return ans;
+}
+
+int main(){
+
+vector<vector<int>> arr = {{1,2,3},{4,5,6},{7,8,9}};
+vector<int> ans = spiralmatrixBuilderFunction(arr);
+
+for(int x : ans){
+    cout<<x<<" ";
+}
+cout<<"\n"; 
 
 return 0;
 }
