9adc27cb101f1fd5150c97725966a0a9d2ddbbe0
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-11-29 00:00:20 +0530

    Spiral Matrix

--- StriverA2Z-Array/31-SpiralMatrix.cpp
+++ StriverA2Z-Array/31-SpiralMatrix.cpp
@@ -10,6 +10,8 @@ int cols = arr[0].size();
 vector<int> ans;
     //top , bottom , left , right 
 
+    if(arr.empty() || arr[0].empty()) return ans;
+
     int top = 0, left = 0;
     int bottom = rows - 1 , right =  cols - 1;
 while(top<= bottom && left <= right)
@@ -33,7 +35,7 @@ while(top<= bottom && left <= right)
     bottom-- ;
     }
     if(left <= right){
-    for(int i= bottom ; i >= top ; i++ ){
+    for(int i= bottom ; i >= top ; i--){
         ans.push_back(arr[i][left]);
     }
     left++;
