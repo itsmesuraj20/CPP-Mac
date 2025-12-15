4d8f2187f2a543cac0aa5dc2df68ecd06b48b025
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-12-15 22:53:11 +0530

    3Sum

--- StriverA2Z-Array/35-3Sum.cpp
+++ StriverA2Z-Array/35-3Sum.cpp
@@ -1,7 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    vector<int> arr = {1,2,3}
+vector<vector<int>> Sumcheck_Brute(vector<int> &arr)
+{
+    int n = arr.size();
+
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == 0)
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k]}; // Possible array ko store kiya hai 
+                    sort(temp.begin(), temp.end()); // Sort kiya jisse redundant value store na ho
+                    st.insert(temp); // set mein insert kr diya simple
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end()); // set internally keeps elements sorted and unique; this line just materializes that into a vector
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> Sumcheck = Sumcheck_Brute(arr);
+
+    for (auto &x : Sumcheck)
+    {
+        for (int i : x)
+        {
+            cout << i << " ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
