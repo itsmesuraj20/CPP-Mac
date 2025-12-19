8f83343dac49f0c0c38d06fbcf78aaeb8115fa83
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-12-20 00:25:43 +0530

    SmallestandSecondSmallest.cpp

---
+++ StriverA2Z-Array/4-V1.SmallestandSecondSmallest.cpp
@@ -0,0 +1,17 @@
+#include<bits.stdc++.h>
+using namespace std;
+#define vector<int> vi
+#define vector<vector<int>> vii
+
+int smallestBrute(vi &arr){
+
+    sort(arr.begin(),end());
+    return arr[0];    
+
+}
+
+int main(){
+    vi arr = {1,2,3,4};
+
+    return 0;
+}
