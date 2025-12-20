44b378f3833dd6863d1b6dfd798b1408bb4cb6d6
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-12-20 13:32:44 +0530

    SmallestandSecondSmallest

--- StriverA2Z-Array/4-V1.SmallestandSecondSmallest.cpp
+++ StriverA2Z-Array/4-V1.SmallestandSecondSmallest.cpp
@@ -1,17 +1,37 @@
-#include<bits.stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define vector<int> vi
-#define vector<vector<int>> vii
+#define vi vector<int> 
+#define vvi vector<vector<int>> 
 
-int smallestBrute(vi &arr){
+int smallestBrute(vi &arr)
+{
+    if(arr.empty()) return INT_MAX; //Handle empty case
+    sort(arr.begin(),arr.end());
+    return arr[0]; 
+    
+    //TC - O(n log n)
+    //SC - O(1) or O(log n)
+}
 
-    sort(arr.begin(),end());
-    return arr[0];    
+int smallestOptimal(vi &arr){
+    int n = arr.size();
 
+    if(arr.empty()) return INT_MAX;
+
+    int smallest = arr[0];
+    for(int i = 1 ; i < n ; i++){
+        if(arr[i]< smallest){
+            smallest = arr[i];
+        }
+    }
+    return smallest;
 }
 
 int main(){
-    vi arr = {1,2,3,4};
+    // vi arr = {9,5,10,4};
+    vi arr = {};
 
+    cout<<smallestBrute(arr)<<endl;
+    cout<<smallestOptimal(arr)<<endl;
     return 0;
 }
