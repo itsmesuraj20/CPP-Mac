717038b5e0936e23c2246a7a6d4de0cc4a580397
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-12-20 14:23:33 +0530

    SmallestandSecondSmallest

--- StriverA2Z-Array/4-V1.SmallestandSecondSmallest.cpp
+++ StriverA2Z-Array/4-V1.SmallestandSecondSmallest.cpp
@@ -1,37 +1,96 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define vi vector<int> 
-#define vvi vector<vector<int>> 
+#define vi vector<int>
+#define vvi vector<vector<int>>
 
 int smallestBrute(vi &arr)
 {
-    if(arr.empty()) return INT_MAX; //Handle empty case
-    sort(arr.begin(),arr.end());
-    return arr[0]; 
-    
-    //TC - O(n log n)
-    //SC - O(1) or O(log n)
+    if (arr.empty())
+        return INT_MAX; // Handle empty case
+    sort(arr.begin(), arr.end());
+    return arr[0];
+
+    // TC - O(n log n)
+    // SC - O(1) or O(log n)
 }
 
-int smallestOptimal(vi &arr){
+int smallestOptimal(vi &arr)
+{
     int n = arr.size();
 
-    if(arr.empty()) return INT_MAX;
+    if (arr.empty())
+        return INT_MAX;
 
     int smallest = arr[0];
-    for(int i = 1 ; i < n ; i++){
-        if(arr[i]< smallest){
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
             smallest = arr[i];
         }
     }
     return smallest;
 }
 
-int main(){
-    // vi arr = {9,5,10,4};
-    vi arr = {};
+int secondSmallestBrute(vi &arr)
+{
+    int n = arr.size();
+    if (n < 2)
+        return INT_MAX;
 
-    cout<<smallestBrute(arr)<<endl;
-    cout<<smallestOptimal(arr)<<endl;
+    sort(arr.begin(), arr.end()); // O(n log n);
+
+    int smallest = arr[0]; // for sure hai ki yahi hai
+
+    for (int i = 1; i < n; i++)
+    {
+        // 5 > 4
+        if (arr[i] > smallest)
+        {
+            return arr[i];
+        }
+    }
+}
+
+int secondSmallestBetter(vi &arr){
+    int n = arr.size();
+    if (n < 2)
+        return INT_MAX;
+
+    int smallest = smallestOptimal(arr);
+
+    int secondSmallest = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > smallest && arr[i] < secondSmallest){
+            secondSmallest = arr[i];
+        } 
+    }
+
+    return secondSmallest;
+    
+}
+
+int secondSmallestOptimal(vi &arr){
+    int n = arr.size();
+    if(n < 2) return INT_MAX;
+
+
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
+
+    
+}
+
+int main()
+{
+    vi arr = {9, 5, 10, 4};
+    // vi arr = {};
+
+    // cout << smallestBrute(arr) << endl;
+    // cout << smallestOptimal(arr) << endl;
+    // cout << secondSmallestBrute(arr) << endl;
+    cout << secondSmallestBetter(arr) << endl;
     return 0;
 }
