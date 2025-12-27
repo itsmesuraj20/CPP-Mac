129e03200bee01ea35352c051839bfb388a50326
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2025-12-27 13:12:59 +0530

    Binary Search with recursion

---
+++ Recursion and Backtracking/Recursion/3-recursion(DivideandConquer).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bSearch(vector<int> &arr, int low, int right, int target)
+{
+
+    if (low > right) {
+    cout << "Not Found" << endl;
+    return;
+    }
+
+    int mid = low + (right - low) / 2;
+
+    if (arr[mid] == target)
+    {
+        cout << "Found" << endl;
+        return;
+    }
+
+    else if (arr[mid] < target)
+
+        bSearch(arr, mid + 1, right, target);
+
+    else
+        bSearch(arr, low, mid - 1, target);
+
+}
+int main()
+{
+    vector<int> arr = {1, 5, 6, 7, 9};
+    int target = 9;
+    int n = arr.size();
+    bSearch(arr, 0,n-1, target);
+    return 0;
+}
