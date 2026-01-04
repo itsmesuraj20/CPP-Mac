4112900a7420c244248aa9b689f42dec9231b241
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-01-04 19:39:31 +0530

    Refactor union_Brute function and clean up code formatting

--- StriverA2Z-Array/11-Unionofthetwosortedarray.cpp
+++ StriverA2Z-Array/11-Unionofthetwosortedarray.cpp
@@ -10,9 +10,16 @@ arr2 = [2,3,3,4,4,5];
 
 o/p = [1,2,3,4,5,6];
 
+
+
+Brute - Optimal (No Better)
 */
 
-vector<int> union_Brute(vector<int> arr1,vector<int> arr2){
+vector<int> union_Brute(vector<int> arr1,vector<int> arr2)
+{
+
+    //Set bnao usme dono array ke element push kar do then wapis  ek new answer array mein push kar do;
+
     vector<int> ans ;
 
     int n = arr1.size(); 
@@ -34,10 +41,15 @@ vector<int> union_Brute(vector<int> arr1,vector<int> arr2){
     }
     
     return ans;
+
+    //TC : O 2(n+m log n*m) + O(3n) = 
+    // TC : O(n log n )
+    //SC : O (n log n)
+
 }
 
 vector<int> union_Optimal(vector<int> arr1,vector<int> arr2){
-
+    //
     vector<int> ans;
 
     int n = arr1.size(); 
@@ -45,7 +57,8 @@ vector<int> union_Optimal(vector<int> arr1,vector<int> arr2){
     int i = 0, j = 0;
 
 
-    while(i< n && j<m){
+    while(i< n && j<m)0-\
+?>g bcdfxv ̑QA2{
         if(arr1[i] <= arr2[j]) {
             if(ans.empty() || ans.back() != arr1[i]){
                 ans.push_back(arr1[i]);
@@ -80,6 +93,7 @@ vector<int> union_Optimal(vector<int> arr1,vector<int> arr2){
 }
 
 int main(){
+
     vector<int> arr1 = {1,2,3,4,5,5,6};
     vector<int> arr2 = {2,3,3,4,4,5};
     
