7383c5cdff5f6bf5547b1d5df6b0aa3004ce80e7
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-01-13 23:16:33 +0530

    Fix formatting in union_Optimal function

--- StriverA2Z-Array/11-Unionofthetwosortedarray.cpp
+++ StriverA2Z-Array/11-Unionofthetwosortedarray.cpp
@@ -65,8 +65,9 @@ vector<int> union_Optimal(vector<int> arr1,vector<int> arr2){
             }
             i++;
         }
-        
-        else{
+
+        else
+        {
            if(ans.empty() || ans.back() != arr2[j]){
                 ans.push_back(arr2[j]);
             }
