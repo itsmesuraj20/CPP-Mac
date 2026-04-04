208469dd65f2c40a17c5c3f3d5716d5ced2ee618
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-04 23:17:09 +0530

    MaxmiumProductSubarray

--- StriverA2Z-Array/37-MaximumProductSubarray.cpp
+++ StriverA2Z-Array/37-MaximumProductSubarray.cpp
@@ -1,8 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+/*
+
+Maximum Product Subarray - 
+
+arr [] = { 2,3,-3,4};
+ans = 6;
+
+
+Keep this is Product not Sum
+
+
+*/
+
 int main (){
 
-    vector<int> arr = {1,2,3,4 };
+    vector<int> arr = {2,3,-3,4};
+
+
     return 0;
 }
