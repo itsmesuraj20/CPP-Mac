0c67d725f4cc94622c21291e0cb9d658d2097ccb
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-11 00:40:19 +0530

    TowerofHanoi.cpp

---
+++ Revision/Towerofhanoi.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int n , int source , int destination , int helper){
+
+    if(n == 0 ) return ;
+
+    if( n == 1 ) cout<< source << " -> " << destination ; 
+
+}
+
+int main(){
+
+
+    return 0 ;
+}
