9f41966fe0dabbf2e4229f96047f9b21aa63018d
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-10 23:23:39 +0530

    Dutch National Flag

--- Revision/dutchnationalflag.cpp
+++ Revision/dutchnationalflag.cpp
@@ -1,9 +1,32 @@
-#include<bits.stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 //Dutch National Flag Algorithm
 int main()
 {
-    vector<int> arr = { };
+    vector<int> arr = {1,2,0,1,2,0,2,2,1,1,0};
 
+    int n = arr.size();
+
+    int low = 0, high = n-1, mid = 0;
+
+    while (mid <= high)
+    {
+        if(arr[mid] == 0){
+            swap(arr[low] ,arr[mid]);
+            low++,mid++;
+        }
+    
+    
+    else if (arr[mid] == 1) mid++;
+    else {
+        swap(arr[mid] , arr[high]);
+        high--;
+    }
+}
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<< " " ; 
+    }
+    
     return 0;
 }
