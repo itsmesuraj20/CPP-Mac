3be747a96abc024746e17b43ee60c476a6d2525b
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-12 01:44:45 +0530

    TowerOfHanoi

--- Revision/Towerofhanoi.cpp
+++ Revision/Towerofhanoi.cpp
@@ -1,25 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(int n , int source , int destination , int helper){
+void solve(int n, int source, int destination, int helper) {
+    if (n == 0) return;
 
-    if(n == 0 ) return ;
+    if (n == 1) {
+        cout << "Move Disk " << source << " to " << destination << endl;
+        return;
+    }
 
-    if( n == 1 ) {
-        cout<< source << " -> " << destination ; 
-        return ; 
-    } 
-
-    solve( n - 1, source , helper , destination )
-    cout<<"Move Disk " << source <<" to " << destination ;
-    solve( n - 1, helper , destination , source);
+    solve(n - 1, source, helper, destination);
+    cout << "Move Disk " << source << " to " << destination << endl;
+    solve(n - 1, helper, destination, source);
 }
 
-int main(){
-
-    int n = 3; 
+int main() {
+    int n;
+    cin >> n;
 
     solve(n, 1, 3, 2);
 
-    return 0 ;
+    return 0;
 }
