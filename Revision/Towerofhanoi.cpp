8e4a0609311b81fe71ae9059a8ca4d6202260da4
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-12 01:56:27 +0530

    TowerOfHanoi

--- Revision/Towerofhanoi.cpp
+++ Revision/Towerofhanoi.cpp
@@ -4,19 +4,18 @@ using namespace std;
 void solve(int n, int source, int destination, int helper) {
     if (n == 0) return;
 
-    if (n == 1) {
-        cout << "Move Disk " << source << " to " << destination << endl;
-        return;
-    }
-
     solve(n - 1, source, helper, destination);
-    cout << "Move Disk " << source << " to " << destination << endl;
+
+    cout << "Move Disk " << n 
+         << " from " << source 
+         << " to " << destination << endl;
+
     solve(n - 1, helper, destination, source);
 }
 
 int main() {
-    int n;
-    cin >> n;
+    int n = 3;
+
 
     solve(n, 1, 3, 2);
 
