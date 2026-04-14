0185e8af441b1189a1a59a289436bef6d8311fbb
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-15 00:55:37 +0530

    Learning : Fibonacci Self

--- Dynamic Programming/Self Learning /fibonaaci.cpp
+++ Dynamic Programming/Self Learning /fibonaaci.cpp
@@ -5,7 +5,7 @@ int fib(int n ){
 
     // TC :O(2^n);
     // SC : O(n);
-    if( n <= 0) return 0;
+    if( n <= 0 ) return 0;
     if( n == 1 ) return 1;
     return fib(n-1)+fib(n-2);
 
@@ -13,7 +13,11 @@ int fib(int n ){
 
 int fib(int n){
 
-    
+    if( n <= 0 ) return 0;
+    if( n == 1 ) return 1;
+
+
+
 }
 
 int main(){
