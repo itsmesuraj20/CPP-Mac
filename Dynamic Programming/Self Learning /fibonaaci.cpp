ad579027f972b4eaf4f9c2564f629add9e5858eb
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-04-15 17:15:29 +0530

    Fibonacci DP

--- Dynamic Programming/Self Learning /fibonaaci.cpp
+++ Dynamic Programming/Self Learning /fibonaaci.cpp
@@ -13,13 +13,32 @@ int fib(int n ){
     return fib(n-1)+fib(n-2);
 }
 
-int fib(int n,vector<int> &dp){
+
+// Memoization : 
+
+int fibMemoization(int n,vector<int> &dp){
     
     if( n <= 1 ) return n;
 
     if( dp[n] != -1 ) return dp[n] ;
 
     return dp[n] = fib(n-1,dp)+fib(n-2,dp);
+
+}
+
+int fibTabulation(int n ,vector<int> &dp)
+{
+    vector<int> dp(n+1);
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n  ; i++)
+    {
+       dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
 
 }
 
@@ -30,7 +49,8 @@ int main(){
     vector<int> dp(n,-1);
 
     cout<<fib(n)<<endl;
-    cout<<fib(n,dp)<<endl;
+    cout<<fibMemoization(n,dp)<<endl;
+    cout<<fibTabulation(n,dp)<<endl;
 
     return 0;
 
