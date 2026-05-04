d6c540e70f52d78f4fa5000cbe316677a3bf69b4
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-05-04 23:58:47 +0530

    Remove unnecessary blank lines and add missing newline at end of file in fibonaaci.cpp

--- Dynamic Programming/Self Learning /fibonaaci.cpp
+++ Dynamic Programming/Self Learning /fibonaaci.cpp
@@ -11,7 +11,6 @@ int fib(int n ){
     if( n <= 1 ) return n;
 
     return fib(n-1)+fib(n-2);
-
 }
 
 
@@ -24,7 +23,6 @@ int fibMemoization(int n,vector<int> &dp){
     if( dp[n] != -1 ) return dp[n] ;
 
     return dp[n] = fib(n-1,dp)+fib(n-2,dp);
-
 }
 
 int fibTabulation(int n ,vector<int> &dp)
@@ -53,5 +51,5 @@ int main(){
     cout<<fibTabulation(n,dp)<<endl;
 
     return 0;
-    
+
 }
