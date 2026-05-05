26b4df2165d3a424b0b8846e8ca7ba6edcccb2bb
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-05-05 11:22:02 +0530

    Remove unnecessary blank lines in fibonaaci.cpp

--- Dynamic Programming/Self Learning /fibonaaci.cpp
+++ Dynamic Programming/Self Learning /fibonaaci.cpp
@@ -11,14 +11,12 @@ int fib(int n ){
     if( n <= 1 ) return n;
 
     return fib(n-1)+fib(n-2);
-
 }
 
 
 // Memoization : 
 
 int fibMemoization(int n,vector<int> &dp){
-    
     if( n <= 1 ) return n;
 
     if( dp[n] != -1 ) return dp[n] ;
