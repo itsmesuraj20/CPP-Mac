0099b42d88392517af3dbe6d09f9e71955dac42d
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-05-20 00:09:24 +0530

    Refactor code structure for improved readability and maintainability

--- practice.cpp
+++ practice.cpp
@@ -6,10 +6,35 @@ vector<int> dutchalgorithm(vector<int> &arr){
 
     int low = 0 , mid = 0 , high = n-1;
 
-    while(mid <= )
+    while(mid <= high){
+        if(arr[mid] == 0){
+            swap(arr[mid] , arr[low]);
+            low ++;
+            high ++;
+        }
 
+        else if(arr[mid] == 1){
+            mid++;
+        }
+
+        else {
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+
+    return arr;
 }
 
 int main(){
-    
+    vector<int> arr ={2,1,0,1,2,0,0,1};
+
+    vector<int> ans = dutchalgorithm(arr);
+
+    for(int i : ans ){
+        cout<< i << " ";
+    }
+
+
+    return 0 ;
 }
