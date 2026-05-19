fda081079ec5e494a17baae033a2eff3306f43d5
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-05-19 23:59:49 +0530

    Add initial implementation of Dutch National Flag algorithm in practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dutchalgorithm(vector<int> &arr){
+    int n = arr.size();
+
+    int low = 0 , mid = 0 , high = n-1;
+
+    while(mid <= )
+
+}
+
+int main(){
+    
+}
