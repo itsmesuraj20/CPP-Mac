94ec5117e4d4aae7311a02956643db3d26160e23
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-06-13 01:31:32 +0530

    Recursion

---
+++ RTGD/Recursion/heightofthetree.cpp
@@ -0,0 +1,21 @@
+//Height of the Binary Tree
+
+class Node {
+    int n ; 
+    Node (int n){
+        int data = n;
+        root = data;
+        left = root->null;
+        right = root ->null;
+    }
+}
+
+int heightofTree(Node *root ,Node *left, Node * ){
+    if(root == Nullptr) return 0;
+
+    int lh = heightofTree(root ->left);
+    int rh = heightofTree (root -> right);
+
+
+    return 1 + max(lh,rh);
+}
