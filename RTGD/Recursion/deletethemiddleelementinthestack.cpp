6fdb09cb746eee104f8e4ec3b3cc895337f775c7
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-06-20 01:09:36 +0530

    Deletethemiddleinthestack

--- RTGD/Recursion/deletethemiddleelementinthestack.cpp
+++ RTGD/Recursion/deletethemiddleelementinthestack.cpp
@@ -5,16 +5,38 @@ void insert(){
 
 }
 
-void delete(stack<int> &st , int k){
-    if(st.empty())
-    return; 
+void delete(stack<int> &st , int k)
+{
+    if(k == 1)
+    {
+        st.pop();
+        return; 
+    }
 
     int val = st.top();
+    st.pop();
+
+    delete(st,k-1);
+
+    insert(st);
 
 }
 
 int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
 
+    delete(st , k);
+
+    while(!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+    }
 
     return 0;
 }
