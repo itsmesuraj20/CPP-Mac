c4836d33d05627d36f482dbc30664316302fbffd
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-06-19 17:08:55 +0530

    RTGD

---
+++ RTGD/Recursion/sortanstack.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+st.push() -> add the element
+st.pop() -> mean delete the element
+st.top() -> top most element 
+st.empty() -> Empty the stack
+
+*/
+
+void insertFunc(stack<int> &arr , int temp){
+
+}
+
+void stackSort(stack<int> & arr){
+    
+    if(arr.empty()){
+        return ;
+    }
+
+
+
+}
+
+
+int main(){
+
+    stack<int> s = {0,1,5,2};
+
+    return 0;
+}
