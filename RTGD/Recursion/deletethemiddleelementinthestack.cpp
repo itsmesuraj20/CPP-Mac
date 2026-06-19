f3a847a7070a9c8a1ca41031c7963fc1e0a40c62
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-06-19 20:43:46 +0530

    deletethemiddleelement - aditya verma

--- RTGD/Recursion/deletethemiddleelementinthestack.cpp
+++ RTGD/Recursion/deletethemiddleelementinthestack.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void insert(){
+
+}
+
+void delete(stack<int> &st , int k){
+    if(st.empty())
+    return; 
+
+    int val = st.top();
+
+}
+
 int main(){
-    
+
+
+    return 0;
 }
