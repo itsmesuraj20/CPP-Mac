7ad9c51bd4922017009d90845b1b6b3bdf8077d3
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-07-15 23:46:35 +0530

    RTGD - Reverse the stack

--- RTGD/Recursion/reverseastack.cpp
+++ RTGD/Recursion/reverseastack.cpp
@@ -1,15 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void insertStack(stack<int> &st , int temp)
+{
+    if(st.empty()){
+        st.push(temp);
+        return ;
+    }
+
+    int top = st.top();
+    st.pop();
+
+    insertStack(st,temp);
+
+    st.push(top);
+
+}
+
+void reverseStack(stack<int> &st){
+
+    if(st.empty()){
+        return ;
+    }
+
+    int topele = st.top();
+    st.pop();
+
+    reverseStack(st);
+
+    insertStack(st,topele);
+
+}
+
 int main(){
-    vector<int> arr = {1,2,3}; 
 
+    // vector<int> arr = {1,2,3};
+     
+    vector<int> arr = {3,2,1}; 
+    
     stack<int> st;
-
 
     //Pushing it arr to stack 
     for(int i :arr){
         st.push(i);
+    }
+
+    reverseStack(st);
+
+    while(!st.empty())
+    {
+
+        cout<< st.top() << " ";
+        st.pop();
+
     }
     
     return 0;
