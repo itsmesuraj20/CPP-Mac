97d3c20cad147a5f37665cb29415d48e732bcec3
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-08-14 02:51:14 +0530

    RTGD , Practice - Practice : Reverse a stack using Recursion

---
+++ Practice/practice1.cpp
@@ -0,0 +1,65 @@
+// Reverse a stack using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+void insertStack(stack<int> &st, int temp)
+{
+
+    if (st.empty())
+    {
+        st.push(temp);
+        return;
+    }
+
+    int temp_insert = st.top();
+    st.pop();
+
+    insertStack(st, temp);
+
+    st.push(temp_insert);
+}
+void reverse(stack<int> &st)
+{
+    if (st.empty())
+    {
+        return;
+    }
+
+    int temp = st.top();
+    st.pop();
+
+    reverse(st);
+
+    insertStack(st, temp);
+}
+int main()
+{
+  stack<int> st;
+
+  st.push(1);
+  st.push(2);
+  st.push(3);
+
+  cout<<"Before : "<<endl;
+  
+  //Creating a copy of the original;
+  stack<int> temp = st;
+  
+  while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    reverse(temp);
+
+    cout << "After : " << endl;
+
+    while (!temp.empty())
+    {
+        cout << temp.top() << endl;
+        temp.pop();
+    }
+
+    return 0;
+}
