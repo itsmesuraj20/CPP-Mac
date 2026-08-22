6302959f5a931df0ceb2187fe4d7ccfd2f2dba77
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-08-22 21:30:38 +0530

    Practice Kth Symbolic Grammer

--- Practice/practice-KthSymbolGrammer.cpp
+++ Practice/practice-KthSymbolGrammer.cpp
@@ -1,7 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int solve(int n , int k){
+    if(n == 1 && k == 1)
+    {
+        return 0;
+    }
+
+    int mid = pow(2, n-1)/ 2; 
+
+    if(k <= mid )
+    return solve(n-1 , k);
+
+    else{
+        return !solve(n-1,k-mid);
+    }
+}
+
 int main(){
+    
+    int n , k ; 
+
+    cin>> n >> k;
+
+    solve(n , k);
+
+    cout<<solve(n,k)<<endl;
 
     return 0;
 }
