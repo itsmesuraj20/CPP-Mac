2ecb8b3e6764ca7f2074fd2f5840b0ccb8d19c30
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-08-24 23:39:55 +0530

    Tower of Hanoi

---
+++ RTGD/Recursion/11video-TowerofHanoi.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(int n , char src , char help ,char desti){
+    int c = 1;
+    if(n == 0){
+        cout<<endl;
+        return;
+    }
+
+    solve(n-1,src,desti,help);
+    cout << c << " : Disk " << n << " from " << src << " to " << desti << endl;
+    c++;
+    solve(n-1,help,src,desti);
+
+}
+int main(){
+    int n = 3 ; 
+    
+    char s = 'a';
+    char h = 'b';
+    char d = 'c';
+
+    solve(n, s,h,d);
+
+    return 0;
+
+}
