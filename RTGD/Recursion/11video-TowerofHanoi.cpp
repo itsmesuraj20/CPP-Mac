4e8e339f51dc7c3cb3355afb4bcc01ac386e6f3e
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-08-26 22:48:44 +0530

    TowerofHanoi

--- RTGD/Recursion/11video-TowerofHanoi.cpp
+++ RTGD/Recursion/11video-TowerofHanoi.cpp
@@ -1,27 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve(int n , char src , char help ,char desti){
-    int c = 1;
+void solve(int n , char src , char help ,char desti, int &c){
+//updated a serial number
     if(n == 0){
         cout<<endl;
         return;
     }
 
-    solve(n-1,src,desti,help);
-    cout << c << " : Disk " << n << " from " << src << " to " << desti << endl;
-    c++;
-    solve(n-1,help,src,desti);
+    solve(n-1,src,desti,help,c);
+    cout << ++c << " : Disk " << n << " from " << src << " to " << desti << endl;
+    solve(n-1,help,src,desti,c);
 
 }
 int main(){
-    int n = 3 ; 
-    
+    int n = 4 ; 
+    int c = 0;
     char s = 'a';
     char h = 'b';
     char d = 'c';
 
-    solve(n, s,h,d);
+    solve(n, s,h,d,c);
 
     return 0;
 
-}`
+}
