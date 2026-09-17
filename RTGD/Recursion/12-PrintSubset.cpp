6bf13ba7c39e3f8eeb3f9b0668e7a3f26b290c3a
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-09-17 23:10:21 +0530

    PrintSubset

--- RTGD/Recursion/12-PrintSubset.cpp
+++ RTGD/Recursion/12-PrintSubset.cpp
@@ -1,1 +1,24 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr = { 1, 2,3};
+
+    int i , j; 
+
+    for (int i = 0; i < j; i++)
+    {
+       for (int j = 0; j < i; j++)
+       {
+            int k = 0;
+            for (int k = 0; k <= j; k++)
+            {
+                
+            }
+            
+       }
+       
+    }
+    
+    return 0;
+}
