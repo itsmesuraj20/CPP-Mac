4d2e9217202ab1983472b8bee107e93dbd241803
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2026-09-23 23:44:03 +0530

    Print Subset with Recursion

--- RTGD/Recursion/12-PrintSubset.cpp
+++ RTGD/Recursion/12-PrintSubset.cpp
@@ -3,22 +3,6 @@ using namespace std;
 
 int main(){
     vector<int> arr = { 1, 2,3};
-
-    int i , j; 
-
-    for (int i = 0; i < j; i++)
-    {
-       for (int j = 0; j < i; j++)
-       {
-            int k = 0;
-            for (int k = i; k <= j; k++)
-            {
-                cout<< k << " ";
-            }
-            cout<< endl;
-       }
-       
-    }
     
     return 0;
 }
